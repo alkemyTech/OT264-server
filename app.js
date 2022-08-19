750f1d08761e6c395893ad1af69f6213fe2c397b
--add <rodrigo.gallardo.6012@gmail.com>
2022-08-19 19:56:11 -0300

    Endpoint datos públicos de la Organización

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const organizationRouter = require('./routes/organization');
 
 const app = express();
 app.use(cors());
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/organization', organizationRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -40,5 +42,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(process.env.PORT, () => console.log('server funcionando'));
 
 module.exports = app;
--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -1,3 +1,17 @@
-class OrganizationController {}
+const { Organization } = require('../models');
+
+class OrganizationController {
+  static async getAll(req, res) {
+    try {
+      const organization = await Organization.findAll({
+        attributes: ['name', 'image', 'phone', 'address']
+      });
+      res.status(200).json(organization);
+    } catch (error) {
+      console.log(error);
+      res.status(404).send('respond with a resource');
+    }
+  }
+}
 
 module.exports = OrganizationController;
