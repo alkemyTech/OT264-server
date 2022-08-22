1c2fad3f9c9081eeb0fad25f16f0ea5205304a2f
Your Name <you@example.com>
2022-08-22 12:01:58 -0300

    adding seeders

---
+++ seeders/20220822134358-demo-testimonial.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkInsert('Testimonials', [{
+      name: 'John Doe',
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      content: 'Testimonial test',
+      createdAt: new Date,
+      updatedAt: new Date
+     }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
