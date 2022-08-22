0395a24676d6f12bde82ac8fa9b9a79bb29a86b8
cdlavacudeg <cristianlav10@gmail.com>
2022-08-22 18:22:21 -0500

    Fix sendgrid config

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,3 @@
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-class UserController {
-  async sendEmail(email, subject, body) {
-    const msg = {
-      to: email, // Change to your recipient
-      from: 'cristianlav10@gmail.com', // Change to your verified sender
-      subject: subject,
-      text: body
-    };
-
-    const response = sgMail.send(msg);
-    return response;
-  }
-}
+class UserController {}
 
 module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const UserController = require('../controllers/userController');
-const userController = new UserController();
-/* GET users listing. */
+
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
-});
-
-router.post('/email', async function (req, res, next) {
-  try {
-    const { email, subject, body } = req.body;
-    await userController.sendEmail(email, subject, body);
-    res.send('Email send');
-  } catch (error) {
-    console.log(error);
-    res.send(error.message);
-  }
 });
 
 module.exports = router;
