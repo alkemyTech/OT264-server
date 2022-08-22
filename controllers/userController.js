db1cf2f845e33aebd0c88a981afbb4722c0dc749
cdlavacudeg <cristianlav10@gmail.com>
2022-08-22 17:42:11 -0500

    Add Send email Sendgrid
    Configuration of sengrid to send emails

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
-class UserController {
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+class UserController {
+  async sendEmail(email, subject, body) {
+    const msg = {
+      to: email, // Change to your recipient
+      from: 'cristianlav10@gmail.com', // Change to your verified sender
+      subject: subject,
+      text: body
+    };
+
+    const response = sgMail.send(msg);
+    return response;
+  }
 }
 
 module.exports = UserController;
