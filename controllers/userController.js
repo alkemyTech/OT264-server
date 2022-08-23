8fc598918ee31eff8d063df802e22e9d144266a8
--add <rodrigo.gallardo.6012@gmail.com>
2022-08-23 00:50:13 -0300

    Endpoint delete users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,18 @@
-class UserController {}
+const { User } = require('../models');
+
+class UserController {
+  static async deleteUser(req, res) {
+    try {
+      const { id } = req.params;
+      const deleteUser = await User.destroy({ where: { id } });
+      if (deleteUser) {
+        return res.status(200).send({ msg: `El usuario fue eliminado` });
+      }
+      return res.status(400).json({ msg: 'Ese usuario no existe' });
+    } catch (error) {
+      res.status(404).json({ msg: 'Ah ocurrido un error' });
+    }
+  }
+}
 
 module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const UserController = require('../controllers/userController');
 
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
