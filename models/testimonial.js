1146e76342d716c9b8f51e79b06251a78ca62365
Your Name <you@example.com>
2022-08-23 11:12:40 -0300

    slides model

--- models/testimonial.js
+++ models/testimonial.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Testimonial extends Model {
     /**
@@ -12,16 +10,19 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Testimonial.init({
-    name: DataTypes.STRING,
-    image: DataTypes.STRING,
-    content: DataTypes.STRING,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Testimonial',
-    paranoid: true
-  });
+  }
+  Testimonial.init(
+    {
+      name: DataTypes.STRING,
+      image: DataTypes.STRING,
+      content: DataTypes.STRING,
+      deletedAt: DataTypes.DATE
+    },
+    {
+      sequelize,
+      modelName: 'Testimonial',
+      paranoid: true
+    }
+  );
   return Testimonial;
 };
