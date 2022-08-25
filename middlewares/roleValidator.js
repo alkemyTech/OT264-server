6d6c1c36e54662b3820627c0f44ba179738628bd
cdlavacudeg <cristianlav10@gmail.com>
2022-08-25 07:58:39 -0500

    Add exeption when user is not authenticated

--- middlewares/roleValidator.js
+++ middlewares/roleValidator.js
@@ -1,7 +1,12 @@
 class RoleValidator {
   static async isAdmin(req, res, next) {
-    const { roleId } = req.user;
-
+    //Verify that user is athenticate (req.user exists)
+    const user = req.user;
+    if (!user) {
+      return res.status(401).send({ msg: 'User must be authenticated' });
+    }
+    const { roleId } = user;
+    //Verify role admin -> In Roles Table id 1 is admin
     if (roleId !== 1) {
       return res.status(401).send({ msg: 'Unauthorized' });
     }
