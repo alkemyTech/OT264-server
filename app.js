bb485e873fcd65c0424368503b29a5b72182b36b
Your Name <you@example.com>
2022-08-29 23:20:20 -0300

    endpoint create activities

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const organizationRouter = require('./routes/organization');
 const authRouter = require('./routes/auth');
+const activitiesRouter = require('./routes/activities');
 const app = express();
 app.use(cors());
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/organization', organizationRouter);
 app.use('/auth', authRouter);
+app.use('/activities', activitiesRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
