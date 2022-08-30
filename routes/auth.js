4843c00fb04762278812c7d2674de0db0f83ba6f
cdlavacudeg <cristianlav10@gmail.com>
2022-08-30 08:49:05 -0500

    Endpoint datos usuario autenticado

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const Validator = require('../middlewares/validator');
+const Authentication = require('../middlewares/authentication');
 
 const UserController = require('../controllers/userController');
 
@@ -22,5 +23,7 @@ router.post(
   Validator.validateField,
   UserController.login
 );
+
+router.get('/me', Authentication.isAuthenticated, UserController.authenticateMe);
 
 module.exports = router;
