54a8449e5e959e87b66a6b7254b55bdfc6a24c50
--add <rodrigo.gallardo.6012@gmail.com>
2022-09-01 16:17:43 -0300

    endpoint update  news

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -3,9 +3,9 @@ const { New, Categories } = require('../models');
 class NewsController {
   static async create(req, res) {
     const data = req.body;
-    const { categoryId } = data;
+    const { categoriesId } = data;
     try {
-      const category = await Categories.findOne({ where: { id: categoryId } });
+      const category = await Categories.findOne({ where: { id: categoriesId } });
       if (!category) {
         throw new Error('Categoria no existe');
       }
@@ -24,6 +24,23 @@ class NewsController {
       news = await New.findByPk(id);
       if (news) {
         return res.status(200).json(news);
+      } else {
+        return res.status(404).json({ msg: 'News not found' });
+      }
+    } catch (error) {
+      return res.status(500).json({ msg: 'Internal Server error' });
+    }
+  }
+  static async updateNews(req, res) {
+    const { id } = req.params;
+    const data = req.body;
+
+    try {
+      const newsUpdated = await New.findByPk(id);
+      if (newsUpdated) {
+        await New.update({ ...data }, { where: { id } });
+        const newsUpdated = await New.findOne({ where: { id } });
+        return res.status(200).json(newsUpdated);
       } else {
         return res.status(404).json({ msg: 'News not found' });
       }
