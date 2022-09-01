376883ab8465adcd189b62d2494f33542ed11b40
Your Name <you@example.com>
2022-09-01 12:05:06 -0300

    OT-59_slide_detail

--- controllers/slidesController.js
+++ controllers/slidesController.js
@@ -1,2 +1,20 @@
-class SlidesController {}
+const { Slide } = require('../models');
+
+class SlidesController {
+  static async getById(req, res) {
+    const { id } = req.params;
+    let slide;
+    try {
+      slide = await Slide.findByPk(id);
+    } catch (error) {
+      return res.status(500).json({ msg: 'Internal Server error' });
+    }
+    if (slide) {
+      return res.status(200).json(slide);
+    } else {
+      return res.status(404).json({ msg: 'Slide not found' });
+    }
+  }
+}
+
 module.exports = SlidesController;
