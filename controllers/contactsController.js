7bcb48b0fedaa5c8a004b50df2182ebd85e54931
cdlavacudeg <cristianlav10@gmail.com>
2022-09-04 12:52:36 -0500

    Send email when a contact is made

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,15 +1,19 @@
 const { Contacts } = require('../models');
-
+const sendEmail = require('../utils/sendEmail');
 class ContactsController {
   static async create(req, res) {
     const data = req.body;
+    const { email } = req.body;
+    let newContact;
     try {
-      const newContact = await Contacts.create(data);
-      res.status(200).send(newContact);
+      newContact = await Contacts.create(data);
+      const subject = 'Formulario de contacto registrado exitosamente';
+      const body = `Agradecemos su contacto con nosotros, nos comunicaremos lo más pronto posible`;
+      await sendEmail(email, subject, body);
     } catch (err) {
-      console.log(err);
-      res.status(500).send({ msg: 'Internal Server error' });
+      return res.status(500).send({ msg: 'Internal Server error' });
     }
+    res.status(200).send(newContact);
   }
 }
 
