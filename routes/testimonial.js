d0a41c08473dfe43a40919fdf8f34e417a6418dc
apscecilia <apscecilia@gmail.com>
2022-09-04 23:23:14 -0300

    Eliminacion de testimonio

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 const TestimonialController = require('../controllers/testimonialsController');
 const RoleValidator = require('../middlewares/roleValidator');
+const Validator = require('../middlewares/validator');
+const { body } = require('express-validator');
 
+router.post(
+  '/',
+  RoleValidator.isAdmin,
+  body('name', 'name required').notEmpty(),
+  body('content', 'content required').notEmpty(),
+  Validator.validateField,
+  TestimonialController.create
+);
 router.put('/:id', RoleValidator.isAdmin, TestimonialController.updateTestimonial);
-
+router.delete('/:id', RoleValidator.isAdmin, TestimonialController.deleteTestimonial);
 module.exports = router;
