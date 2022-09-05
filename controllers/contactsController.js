f2a07ca68668917fca8f438331f7ed6dc6615933
cdlavacudeg <cristianlav10@gmail.com>
2022-09-05 18:50:05 -0500

    Fix - Send Email and reuse emailBienvenida.ejs

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,15 +1,23 @@
 const { Contacts } = require('../models');
 const sendEmail = require('../utils/sendEmail');
+const ejs = require('ejs');
+const path = require('path');
+
 class ContactsController {
   static async create(req, res) {
-    const data = req.body;
-    const { email } = req.body;
+    const { name, phone, email, message } = req.body;
+    const data = { name, phone, email, message };
     let newContact;
     try {
       newContact = await Contacts.create(data);
+
+      const html = await ejs.renderFile(path.join(__dirname, '../views/emailBienvenida.ejs'), {
+        mensajeBienvenida: 'Agradecemos su contacto con nosotros, nos comunicaremos lo más pronto posible',
+        ongContacto: ['Mail: somosfundacionmas@gmail.com', 'Teléfono de contacto: 1160112988']
+      });
+
       const subject = 'Formulario de contacto registrado exitosamente';
-      const body = `Agradecemos su contacto con nosotros, nos comunicaremos lo más pronto posible`;
-      await sendEmail(email, subject, body);
+      await sendEmail(email, subject, 'Formulario de contacto registrado', html);
     } catch (err) {
       return res.status(500).send({ msg: 'Internal Server error' });
     }
