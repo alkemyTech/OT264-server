4e8aa0489963756ba5081e8c86d5435a4df09b1e
--add <rodrigo.gallardo.6012@gmail.com>
2022-09-06 18:49:25 -0300

    enpoint updateActivities

--- routes/activities.js
+++ routes/activities.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const { body } = require('express-validator');
+const Validator = require('../middlewares/validator');
 const ActivitiesController = require('../controllers/activitiesController');
 const RoleValidator = require('../middlewares/roleValidator');
 
 router.post('/', RoleValidator.isAdmin, ActivitiesController.create);
+router.put(
+  '/:id',
+  RoleValidator.isAdmin,
+  body('name', 'name required').notEmpty(),
+  body('content', 'content required').notEmpty(),
+  body('image', 'image required').notEmpty(),
+  Validator.validateField,
+  ActivitiesController.updateActivities
+);
 
 module.exports = router;
