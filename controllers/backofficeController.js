4be5ed889df409116e82ef247826a22ea2d26fea
apscecilia <apscecilia@gmail.com>
2022-09-10 18:06:01 -0300

    tabla de contacto ok

--- controllers/backofficeController.js
+++ controllers/backofficeController.js
@@ -1,26 +1,14 @@
 const { Contacts } = require('../models');
-//const backoffice = require('../views/backofficeContacts');
-const path = require('path');
-const http = require('http');
-//const fs = require('fs');
+const { NotFound } = require('../utils/error');
 
 class BackofficeController {
-  /*static async getAll(name, phone, email, message, req, res) {
-    const back = await res.sendFile(path.join(__dirname, '../views/backofficeContacts.html'));
-    /*const back = await html.renderFile(path.join(__dirname, '../views/backofficeContacts.html'), {
-      nombre: name,
-      telefono: phone,
-      correo: email,
-      mensaje: message
-    });
-    if (back) {
-      res.writeHead(200, { 'Content-Type ': ' text/html' });
-    }
-  }*/
-  static async getAll(req, res) {
-    const contact = await Contacts.findAll();
-    if (contact) {
-      res.json(contact);
+  static async getContacts(req, res) {
+    try {
+      const contacts = await Contacts.findAll();
+      const parseContacts = JSON.parse(JSON.stringify(contacts));
+      res.render('backofficeContacts', { title: 'Contacts', contacts: parseContacts });
+    } catch (err) {
+      return NotFound;
     }
   }
 }
--- routes/backoffice.js
+++ routes/backoffice.js
@@ -4,10 +4,6 @@ const IsRole = require('../middlewares/roleValidator');
 
 const BackofficeController = require('../controllers/backofficeController');
 
-//router.get('/contacts', IsRole.isAdmin, BackofficeController.getAll);
-router.get('/contacts', BackofficeController.getAll);
-/*router.get('/contact', (req, res) => {
-  res.sendFile(this.patch.resolver(__dirname, '../views/backofficeContacts.html'));
-});*/
+router.get('/contacts', IsRole.isAdmin, BackofficeController.getContacts);
 
 module.exports = router;
