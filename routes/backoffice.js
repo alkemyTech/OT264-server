cafc44608e04805f63349bf9dc235215bf1f6534
cdlavacudeg <cristianlav10@gmail.com>
2022-09-13 07:49:01 -0500

    Fix Typo - RoleValidator middleware
    Estandar name for the middleware

--- routes/backoffice.js
+++ routes/backoffice.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const IsRole = require('../middlewares/roleValidator');
+const RoleValidator = require('../middlewares/roleValidator');
 
 const BackofficeController = require('../controllers/backofficeController');
 
-router.get('/contacts', IsRole.isAdmin, BackofficeController.getContacts);
+router.get('/contacts', RoleValidator.isAdmin, BackofficeController.getContacts);
 
 module.exports = router;
