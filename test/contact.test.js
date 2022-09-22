3d72baa5c0965ad22397c7d3b901849628c4ef96
apscecilia <apscecilia@gmail.com>
2022-09-21 23:23:35 -0300

    test  endpoint contacto

--- test/contact.test.js
+++ test/contact.test.js
@@ -8,7 +8,7 @@ const request = require('supertest')(app);
 const expect = chai.expect;
 const baseUrl = '/contacts';
 
-describe('Se testean los endpoint - caso feliz', () => {
+describe('Contact endpoint test', () => {
   const sandbox = sinon.createSandbox();
   before(() => {
     sandbox.stub(JwtUtils, 'verifyToken').resolves({
@@ -22,6 +22,14 @@ describe('Se testean los endpoint - caso feliz', () => {
     sinon.restore();
     sandbox.restore();
   });
+
+  const contact = {
+    id: 1,
+    name: 'contact1',
+    phone: '1234567891',
+    email: 'contact1@email.com',
+    message: 'mensaje contacto 1'
+  };
 
   describe('GET /contacts', () => {
     it('It should get all contacts', async () => {
@@ -42,84 +50,24 @@ describe('Se testean los endpoint - caso feliz', () => {
         }
       ];
       sandbox.stub(Contacts, 'findAll').resolves(contacts);
-      const response = await request.get(baseUrl).send(contacts);
+      const response = await request.get(baseUrl);
       expect(response.status).to.be.equal(200);
+      expect(response.body.contacts);
     });
   });
 
   describe('POST /contacts', () => {
     it('return insert a contact should succeed', async () => {
-      const response = await request.post(baseUrl).send({
-        id: 1,
-        name: 'contact1',
-        phone: '1234567891',
-        email: 'contact1@email.com',
-        message: 'mensaje contacto 1'
-      });
-      expect(response.status).to.eql(200);
-    });
-  });
-  describe('POST /contacts', () => {
-    it('Return insert a contact should fail validation fields', async () => {
-      const response = await request.post(baseUrl).send({
-        id: 1,
-        name: 123,
-        phone: '1234567891',
-        email: '',
-        message: 'mensaje contacto 1'
-      });
-      expect(response.status).to.eql(400);
-    });
-  });
-});
-describe('Solicitar error en caso no feliz', () => {
-  const sandbox = sinon.createSandbox();
-  before(() => {
-    sandbox.stub(JwtUtils, 'verifyToken').resolves({
-      email: 'anEmailValidated@test.com'
-    });
-    sandbox.stub(UserController, 'getByEmail').resolves({
-      roleId: 2
-    });
-  });
-  after(() => {
-    sinon.restore();
-    sandbox.restore();
-  });
-
-  describe('GET /contacts', () => {
-    it('Return get all contacts should fail  without credentials', async () => {
-      const contacts = [
-        {
-          id: 1,
-          name: 'contact1',
-          phone: '1234567891',
-          email: 'contact1@email.com',
-          message: 'mensaje contacto 1'
-        },
-        {
-          id: 2,
-          name: 'contact2',
-          phone: '1234567892',
-          email: 'contact2@email.com',
-          message: 'mensaje contacto 2'
-        }
-      ];
-      sandbox.stub(Contacts, 'findAll').resolves(contacts);
-      const response = await request.get(baseUrl);
-      expect(response.status).to.be.equal(401);
-    });
-  });
-  describe('POST /contacts', () => {
-    it('Return insert a contact should fail with rol user', async () => {
-      const response = await request.post(baseUrl).send({
-        id: 1,
-        name: 'contact1',
-        phone: '1234567891',
-        email: 'contact1@email.com',
-        message: 'mensaje contacto 1'
-      });
-      expect(response.status).to.eql(401);
+      sandbox.stub(Contacts, 'create').resolves(contact);
+      const response = await request.post(baseUrl).send(contact);
+      const newContact = response.body;
+      expect(newContact).to.be.a('object');
+      expect(newContact).to.have.property('name');
+      expect(newContact).to.have.property('phone');
+      expect(newContact).to.have.property('email');
+      expect(newContact).to.have.property('message');
+      expect(response.status).to.be.equal(200);
+      expect(contact).to.be.deep.equal(newContact);
     });
   });
 });
